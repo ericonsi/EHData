9f6410d127ea54924f3fe1a8201c5243d8787501
Eric Hirsch <ericonsi@yahoo.com>
2022-02-24 19:30:58 -0500

    x

--- R/EHDataR.R
+++ R/EHDataR.R
@@ -362,7 +362,7 @@ EHExplore_TwoCategoricalColumns_Barcharts <- function(df, y)
                 position = position_stack(vjust = 0.5))
       
     p <- eval(substitute(p, list(i=i)))
-    plot_list[[i]] <- p 
+    plot_list4[[i]] <- p 
   }
  
   return (plot_list4)
