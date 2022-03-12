f43273786b1d9a17f2f2b9df1568617827e4b622
Eric Hirsch <ericonsi@yahoo.com>
2022-03-12 13:09:33 -0500

    x

--- R/EHDataR.R
+++ R/EHDataR.R
@@ -201,7 +201,7 @@ EHExplore_OneContinuousAndOneCategoricalColumn_Boxplots <- function(df, x)
   
   for(i in 1:ncol(df)) {
     
-    ct <- cor.test(df[,i], df[,x])
+    #ct <- cor.test(df[,i], df[,x])
     
     #xText <- str_c("Correlation: ", round(ct$estimate,2), "   p value: ", round(ct$p.value,2))
     xText <- "hi!"
