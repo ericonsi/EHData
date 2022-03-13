419b43430ed6b53c7c9056f3509aa221278ce5aa
Eric Hirsch <ericonsi@yahoo.com>
2022-03-13 18:53:23 -0400

    x

--- R/EHDataR.R
+++ R/EHDataR.R
@@ -359,7 +359,7 @@ EHExplore_Multicollinearity <-function(df, run_all=FALSE, title="Heatmap for Mul
   cor_res <- cor(my_matrix, use = "na.or.complete")
   
   if (run_all) {
-    pairs.panels(df)
+    psych::pairs.panels(df)
     print(dfCor)
     corrplot(cor_res, method = 'number')
   }
@@ -438,4 +438,60 @@ EHExplore_TwoCategoricalColumns_Barcharts <- function(df, y)
   }
   
   return (plot_list4)
+}
+
+EHModel_Regression_Logistic(df, y, splitRatio = .8)
+{
+  library(caTools)
+  library(ROCR)
+  
+  split <- sample.split(df, SplitRatio = SplitRatio)
+  split
+  
+  train_reg <- subset(df, split == "TRUE")
+  test_reg <- subset(df, split == "FALSE")
+  
+  logistic_model <- glm(fla,
+                        data = df,
+                        family = "binomial")
+  print(logistic_model)
+  
+  # Summary
+  print(summary(logistic_model))
+  
+  # Predict test data based on model
+  predict_reg <- predict(logistic_model,
+                         test_reg, type = "response")
+  predict_reg
+  
+  # Changing probabilities
+  predict_reg <- ifelse(predict_reg >0.5, 1, 0)
+  
+  # Evaluating model accuracy
+  # using confusion matrix
+  table(test_reg$vs, predict_reg)
+  
+  missing_classerr <- mean(predict_reg != test_reg$vs)
+  print(paste('Accuracy =', 1 - missing_classerr))
+  
+  # ROC-AUC Curve
+  ROCPred <- prediction(predict_reg, test_reg$vs)
+  ROCPer <- performance(ROCPred, measure = "tpr",
+                        x.measure = "fpr")
+  
+  auc <- performance(ROCPred, measure = "auc")
+  auc <- auc@y.values[[1]]
+  auc
+  
+  # Plotting curve
+  plot(ROCPer)
+  plot(ROCPer, colorize = TRUE,
+       print.cutoffs.at = seq(0.1, by = 0.1),
+       main = "ROC CURVE")
+  abline(a = 0, b = 1)
+  
+  auc <- round(auc, 4)
+  legend(.6, .4, auc, title = "AUC", cex = 1)
+  
+  return(logistic_model)
 }
