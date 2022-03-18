2c4c2b03cd4c8cd985ab72e5f7d8318cdf14055f
Eric Hirsch <ericonsi@yahoo.com>
2022-03-18 19:47:17 -0400

    x

--- R/EHDataR.R
+++ R/EHDataR.R
@@ -147,7 +147,7 @@ EHExplore_Interactions_Scatterplots <- function(df, y, interaction) {
   
   df[,interaction] <- as.factor(df[,interaction])
   
-  xtext = paste("Value counts: ", aggregate(data.frame(count = v), list(value = v), length))
+  xtext = paste("Value counts: ", aggregate(data.frame(count = df[,interaction]), list(value = df[, interaction]), length))
   
   plot_list <- list()
   
