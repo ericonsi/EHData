cad9e7e342528f18886a88ce349ca98d0571da87
Eric Hirsch <ericonsi@yahoo.com>
2022-10-16 08:05:58 -0400

    x

--- R/EHDataR.R
+++ R/EHDataR.R
@@ -1018,6 +1018,10 @@ EHModel_RandomForest <- function(df4, target, seed=042760, categorical=TRUE, pri
   
 
   x <- as.data.frame(cbind(dfPred, dfEval[,targ123]))
+  x <- x %>%
+    mutate(residuals <- dfEval[,targ123]- predicitons) %>%
+    rename(dfEval[,targ123], "actual")
+  
   newList <- list("rf" = rf, "errors" = x)
   return(newList)
   
