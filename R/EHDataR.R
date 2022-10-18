4411594c3a0e09569ac9300519442771839d68f7
Eric Hirsch <ericonsi@yahoo.com>
2022-10-17 21:44:42 -0400

    x

--- R/EHDataR.R
+++ R/EHDataR.R
@@ -1088,7 +1088,9 @@ EHModel_SVM <- function(df4, target, method = "linear", seed=042760, printSVM = 
   
   method2 <- paste0("svm", method1)
   Formula  = reformulate(".",response=targ123)
-  svm <- train(Formula, data=dfTrain, method=method2, trControl = tc, preProcess = c("center","scale"), tuneGrid = expand.grid(C = seq(0, 2, length = 20)))
+  #svm <- train(Formula, data=dfTrain, method=method2, trControl = tc, preProcess = c("center","scale"), tuneGrid = expand.grid(C = seq(0, 2, length = 20)))
+  svm <- train(Formula, data=dfTrain, method=method2, trControl = tc, preProcess = c("center","scale"))
+  
   
   if (printSVM){
     print(svm)
