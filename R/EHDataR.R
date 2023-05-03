7e6cb0284946a9d28650949ff30eb4237138e972
Eric Hirsch <ericonsi@yahoo.com>
2023-05-03 12:58:41 -0400

    x

--- R/EHDataR.R
+++ R/EHDataR.R
@@ -911,8 +911,10 @@ EHPrepare_BoxCox2 <- function(df, col, print=TRUE, newcol=FALSE)
     hist(df2[,col], main=paste(col, "- Before"))
   }
   
-  .env <- environment() ## identify the environment
   fla7 <- substitute(n ~ 1, list(n = as.name(col)))
+  
+  fla36 <- substitute(n ~ ., list(n = as.name(col)))
+  m3 <- lm(fla36, df2)
   
   fla2 <- substitute(n ~ 1, list(n = as.name(col)))
   m2 <- lm(fla2, df2)
