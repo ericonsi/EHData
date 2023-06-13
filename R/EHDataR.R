c1d3e26abca47ad1d023450ad1271edc828561e6
Eric Hirsch <ericonsi@yahoo.com>
2023-06-13 13:23:04 -0400

    x

--- R/EHDataR.R
+++ R/EHDataR.R
@@ -219,7 +219,7 @@ EHSummarize_SingleColumn_BarCharts <- function(df, font_size=7)
                            scale_fill_brewer(type = "div", palette = 8)+  
                            theme(legend.position="none") +
                            ggtitle(colnames(df)[i]) +
-                           theme(title = element_text(size =(font_size)), axis.title.x = element_blank(), axis.title.y = element_text(size = font_size), axis.text.x = element_text(size = font_size), axis.text.y = element_text(size = font_size, angle=30, vjust=.5), axis.ticks.x = element_blank(), panel.grid.major.x = element_blank(), panel.grid.minor.x=element_blank(), panel.grid.minor.y=element_blank(), panel.grid.major.y=element_line(color="ivory"), panel.background = element_rect(fill = "ivory")) +
+                           theme(title = element_text(size =(font_size)), axis.title.x = element_blank(), axis.title.y = element_text(size = font_size), axis.text.x = element_text(size = font_size, angle=30, vjust=.5), axis.text.y = element_text(size = font_size), axis.ticks.x = element_blank(), panel.grid.major.x = element_blank(), panel.grid.minor.x=element_blank(), panel.grid.minor.y=element_blank(), panel.grid.major.y=element_line(color="ivory"), panel.background = element_rect(fill = "ivory")) +
                            geom_text(aes(label = Count), size=(font_size-4), fontface="bold", color="black",
                                      vjust = 1), list(i=i)))
     
