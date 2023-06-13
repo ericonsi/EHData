426580dcee4aabf520bab8a3fac7b73b270c43ed
Eric Hirsch <ericonsi@yahoo.com>
2023-06-13 16:04:39 -0400

    x

--- R/EHDataR.R
+++ R/EHDataR.R
@@ -256,7 +256,7 @@ EHSummarize_SingleColumn_BarCharts2 <- function(df, font_size=7)
                            ggtitle(colnames(df)[i]) +
                            theme(title = element_text(size =(font_size)), axis.title.x = element_blank(), axis.title.y = element_text(size = font_size), axis.text.x = element_text(size = font_size), axis.text.y = element_text(size = font_size), axis.ticks.x = element_blank(), panel.grid.major.x = element_blank(), panel.grid.minor.x=element_blank(), panel.grid.minor.y=element_blank(), panel.grid.major.y=element_line(color="ivory"), panel.background = element_rect(fill = "ivory")) +
                            geom_text(aes(label = Count), size=(3), fontface="bold", color="black",
-                                     vjust = 1), list(i=i)))
+                                     hjust = 1), list(i=i)))
     
     plot_list2[[i]] <- p 
     
