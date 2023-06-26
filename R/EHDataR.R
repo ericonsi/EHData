67d6a99e5c815d44ba35aea49afeb4f59c1d1959
ericonsi <ericonsi@yahoo.com>
2023-06-26 17:21:45 -0400

    x

--- R/EHDataR.R
+++ R/EHDataR.R
@@ -219,7 +219,7 @@ EHSummarize_SingleColumn_BarCharts1 <- function(df, font_size=7)
                            scale_fill_brewer(type = "div", palette = 8)+  
                            theme(legend.position="none") +
                            ggtitle(colnames(df)[i]) +
-                           theme(title = element_text(size =(font_size)), axis.title.x = element_blank(), axis.title.y = element_text(size = font_size), axis.text.x = element_text(size = font_size, angle=30, vjust=.5), axis.text.y = element_text(size = font_size), axis.ticks.x = element_blank(), panel.grid.major.x = element_blank(), panel.grid.minor.x=element_blank(), panel.grid.minor.y=element_blank(), panel.grid.major.y=element_line(color="ivory"), panel.background = element_rect(fill = "ivory")) +
+                           theme(title = element_text(size =(font_size)), axis.title.x = element_blank(), axis.title.y = element_text(size = font_size), axis.text.x = element_text(size = font_size, angle=30, vjust=.5), axis.text.y = element_text(size = font_size), axis.ticks.x = element_blank(), panel.grid.major.x = element_blank(), panel.grid.minor.x=element_blank(), panel.grid.minor.y=element_blank(), panel.grid.major.y=element_line(color="darkslategray"), panel.background = element_rect(fill = "slategray2")) +
                            geom_text(aes(label = Count), size=(3), fontface="bold", color="black",
                                      vjust = 1), list(i=i)))
     
@@ -257,7 +257,7 @@ EHSummarize_SingleColumn_BarCharts2 <- function(df, font_size=7, decreasingOrder
                            geom_col() +
                            theme(legend.position="none") +
                            ggtitle(colnames(df)[i]) +
-                           theme(title = element_text(size =(font_size), face="bold"), axis.title.x = element_blank(), axis.title.y = element_blank(), axis.text.x = element_text(size = font_size), axis.text.y = element_text(size = font_size), axis.ticks.x = element_blank(), axis.ticks.y = element_blank(), panel.grid.major.x = element_blank(), panel.grid.minor.x=element_blank(), panel.grid.minor.y=element_blank(), panel.grid.major.y=element_line(color="ivory"), panel.background = element_rect(fill = "ivory")) +
+                           theme(title = element_text(size =(font_size), face="bold"), axis.title.x = element_blank(), axis.title.y = element_blank(), axis.text.x = element_text(size = font_size), axis.text.y = element_text(size = font_size), axis.ticks.x = element_blank(), axis.ticks.y = element_blank(), panel.grid.major.x = element_blank(), panel.grid.minor.x=element_blank(), panel.grid.minor.y=element_blank(), panel.grid.major.y=element_line(color="darkslategray"), panel.background = element_rect(fill = "slategray2")) +
                            geom_text(aes(label = Count), size=(3), fontface="bold", color="black",
                                      hjust = 1.5), list(i=i)))
     
