5d48f8d6eb1a449f97fb32f0098fd93ae3825d74
Eric Hirsch <ericonsi@yahoo.com>
2024-05-07 14:07:59 -0400

    x

--- R/EHDataR.R
+++ R/EHDataR.R
@@ -355,6 +355,36 @@ EHSummarize_SingleColumn_Countplots <- function(df, font_size=7)
   return (plot_list2)
 }
 
+EHSummarize_SingleColumn_Countplots2 <- function(df, font_size=7)
+{  
+  df <- select_if(df, is.character)
+  
+  plot_list2 <- list()
+  
+  
+  for(i in 1:ncol(df)) {   
+    
+    df2 <- df |>
+      group_by(d[,i]) |>
+      dplyr::summarize(n=n())
+    
+    p <- eval(substitute(ggplot(df2, aes(x=df[,i], y=n)) +
+                           coord_flip() +  
+                           xlab(colnames(df)[i])  +
+                           #ylab(qk) +
+                           theme(axis.title.x = element_text(size = font_size), axis.title.y = element_text(size = 9), axis.text.x = element_blank(), axis.ticks.x = element_blank(), panel.grid.major.x = element_blank(), panel.grid.minor.x=element_blank(), panel.grid.minor.y=element_blank(), panel.grid.major.y=element_line(color="gray"), panel.background = element_rect(fill = "slategray2")) +
+                           #geom_text(size = 3, position = position_stack(vjust = 0.5)) +
+                           #geom_bar(stat = "identity", col="white", border="black"), list(i=i)))
+                           geom_bar(color="black", fill="white", stat="identity") +
+                           geom_text(aes(label = n), hjust = -.1), list(i=i)))
+    
+    plot_list2[[i]] <- p 
+    
+    
+  }
+  return (plot_list2)
+}
+
 EHSummarize_SingleColumn_Boxplots <- function(df, font_size=7)
 {  
   df <- select_if(df, is.numeric)
